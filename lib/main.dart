b0a9fc90948ab4f00a33acb817f29e967117d656
Daria Dobszai <kalashnikovadoc@gmail.com>
2020-11-03 16:18:25 +0100

    refactor imports

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:photo_app/bloc/photo_bloc.dart';
 import 'package:photo_app/route/generate_route.dart';
-import 'package:photo_app/route/routes.dart';
+import 'package:photo_app/route/route_names.dart';
 
 void main() {
   runApp(MyApp());
